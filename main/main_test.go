2b75592b7c74ef143ea327d3a25625ecfe0601ae
Michael Whatcott <mike@smarty.com>
2024-10-29 10:38:40 -0600

    Finish test suite; extract helper.

--- main/main_test.go
+++ main/main_test.go
@@ -8,35 +8,55 @@ import (
 	"github.com/mdw-smarty-cohort-b-2024-10/calc-lib"
 )
 
+func assertErr(t *testing.T, actual error, targets ...error) {
+	for _, target := range targets {
+		if !errors.Is(actual, target) {
+			t.Helper()
+			t.Errorf("want: %v, got: %v", target, actual)
+		}
+	}
+}
+
 func TestHandler_TwoArgsRequired(t *testing.T) {
 	handler := NewHandler(nil, nil)
 	err := handler.Handle(nil)
-	if !errors.Is(err, errWrongArgCount) {
-		t.Errorf("want: %v, got: %v", errWrongArgCount, err)
-	}
+	assertErr(t, err, errWrongArgCount)
 }
 func TestHandler_FirstArgInvalid(t *testing.T) {
 	handler := NewHandler(nil, nil)
 	err := handler.Handle([]string{"INVALID", "42"})
-	if !errors.Is(err, errInvalidArgument) {
-		t.Errorf("want: %v, got: %v", errInvalidArgument, err)
-	}
+	assertErr(t, err, errInvalidArgument)
 }
 func TestHandler_SecondArgInvalid(t *testing.T) {
 	handler := NewHandler(nil, nil)
 	err := handler.Handle([]string{"42", "INVALID"})
-	if !errors.Is(err, errInvalidArgument) {
-		t.Errorf("want: %v, got: %v", errInvalidArgument, err)
-	}
+	assertErr(t, err, errInvalidArgument)
 }
 func TestHandler_ResultWrittenToOutput(t *testing.T) {
 	stdout := &bytes.Buffer{}
 	handler := NewHandler(stdout, &calc.Addition{})
+
 	err := handler.Handle([]string{"2", "3"})
-	if err != nil {
-		t.Errorf("want: %v, got: %v", nil, err)
-	}
+
+	assertErr(t, err, nil)
 	if stdout.String() != "5" {
 		t.Errorf("want: %v, got: %v", "5", stdout.String())
 	}
+}
+func TestHandler_OutputWriterError(t *testing.T) {
+	boink := errors.New("boink")
+	stdout := &ErringWriter{err: boink}
+	handler := NewHandler(stdout, &calc.Addition{})
+
+	err := handler.Handle([]string{"2", "3"})
+
+	assertErr(t, err, errOutputFailure, boink)
+}
+
+type ErringWriter struct {
+	err error
+}
+
+func (this *ErringWriter) Write([]byte) (n int, err error) {
+	return 0, this.err
 }
